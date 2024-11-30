4f40286ae8f082cb498f4d9fb11ce5a9f2da3740
vaibhavkolase20 <vaibhavkolase20@gmail.com>
2024-11-30 12:12:19 +0530

    v3

---
+++ main.java
@@ -0,0 +1,7 @@
+import java util.Date;
+public class Main {
+    public static void main(Srting[] args) {
+    Date currentDate = new Date();
+    System.out.println("jay shree ram curremt code:" + currentDate);
+    }
+}
