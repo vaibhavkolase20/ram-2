ba0fbff1f494047bb6b3d159dc265165436ff9b2
vaibhavkolase20 <vaibhavkolase20@gmail.com>
2024-12-29 15:26:08 +0530

    Update maven.groovy

--- jenkins file/maven.groovy
+++ jenkins file/maven.groovy
@@ -14,6 +14,13 @@
 
  5) after moving directory go to /opt and edit vim /etc/profile (add there this contain at last)
     export PATH$PATH: '/opt/apache-maven/bin'  
+   [ if this step is faile and your mvn comand don't show version then use step2]
+    step2 [ nano ~/.bashrc
+           export M2_HOME=/opt/maven
+           export MAVEN_HOME=/opt/maven
+           export PATH=${M2_HOME}/bin:${PATH}
+           source ~/.bashrc ] 
+
 
  6) and check version 
    mvn --version
@@ -23,6 +30,6 @@
   mvn archetype:generate -DarchetypeArtifactId=maven-archetype-quickstart -DinteractiveMode=false -DgroupId=my.package.path -DartifactId=myartifac
 
  8) this is the link of how to install maven in ubuntu
-  https://phoenixnap.com/kb/install-maven-on-ubuntu
+  https://www.geeksforgeeks.org/how-to-install-maven-on-linux/ 
 
 
