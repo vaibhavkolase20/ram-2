3030d9cfe57e8bf4698c611282b2abc8f410fb2f
vaibhavkolase20 <vaibhavkolase20@gmail.com>
2025-01-09 12:21:50 +0530

    demo3

--- jenkins file/terraform/ec2.tf
+++ jenkins file/terraform/ec2.tf
@@ -1,14 +1,15 @@
 provider "aws" {
   region = "us-east-1"
 }
+
 resource "aws_instance" "myserver" {
-    ami = "ami-01816d07b1128cd2d"
-    key_name = "id_rsa" 
-    instance_type = "t2.micro"
-    vpc_security_group_ids = [ "sg-0d34c3d2b6fa6492a" ]
-    availability_zone = "us-east-1a"
-    tags = {
-      name = "suraj"
-      env = "dev"
-    }
-}
+  ami             = "ami-0e2c8caa4b6378d8c"  
+  key_name        = "vaibhav.key.pem"
+  instance_type   = "t2.micro"
+  vpc_security_group_ids = ["sg-0a7c002efafb49d30"]
+  availability_zone = "us-east-1b"
+
+  tags = {
+    Name = "ram"
+    Env  = "krushna"
+  }
