f261201eed1bdfd84b9a49880b7a52ba1b45f002
vaibhavkolase20 <vaibhavkolase20@gmail.com>
2025-01-09 11:00:08 +0530

    teraform

---
+++ jenkins file/terraform/installation.tf
@@ -0,0 +1,13 @@
+# how to install terraform in ububtu
+1) create ubuntu ec2 instant
+2) apt-get update
+
+3) wget -O- https://apt.releases.hashicorp.com/gpg | gpg --dearmor | sudo tee /usr/share/keyrings/hashicorp-archive-keyring.gpg
+
+4) echo "deb [signed-by=/usr/share/keyrings/hashicorp-archive-keyring.gpg] https://apt.releases.hashicorp.com $(lsb_release -cs) main" | sudo tee /etc/apt/sources.list.d/hashicorp.list
+
+5) sudo apt update
+
+6) sudo apt install terraform
+
+7) terraform -version
