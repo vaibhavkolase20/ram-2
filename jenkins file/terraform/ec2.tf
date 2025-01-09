9e4a76960c4a1a3e2f40be5a4d6721ec39f2fe42
vaibhavkolase20 <vaibhavkolase20@gmail.com>
2025-01-09 12:01:41 +0530

    demo2

--- jenkins file/terraform/ec2.tf
+++ jenkins file/terraform/ec2.tf
@@ -1,18 +1,14 @@
+provider "aws" {
+  region = "us-east-1"
+}
 resource "aws_instance" "myserver" {
-  ami             = "ami-0e2c8caa4b6378d8c"
-  key_name        = "vaibhav"
-  instance_type   = "t2.micro"
-  vpc_security_group_ids = ["sg-0a7c002efafb49d30"]
-  availability_zone = "us-east-1b"
-
-  tags = {
-    Name = "ram"
-    Env  = "krushna"
-  }
-
-  provisioner "remote-exec" {
-    inline = [
-      "echo 'deb [signed-by=/usr/share/keyrings/hashicorp-archive-keyring.gpg] https://apt.releases.hashicorp.com $(lsb_release -cs) main' | sudo tee /etc/apt/sources.list.d/hashicorp.list"
-    ]
-  }
+    ami = "ami-01816d07b1128cd2d"
+    key_name = "id_rsa" 
+    instance_type = "t2.micro"
+    vpc_security_group_ids = [ "sg-0d34c3d2b6fa6492a" ]
+    availability_zone = "us-east-1a"
+    tags = {
+      name = "suraj"
+      env = "dev"
+    }
 }
